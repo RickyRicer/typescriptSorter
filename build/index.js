2e3aa6ef9395815dd2e95cc0716526adaf823c54
Rick Ansay <rick.ansay@gmail.com>
2022-06-11 16:21:32 -0600

    First commit

---
+++ build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log('hello there');
+const logSomething = () => {
+    console.log('something');
+};
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+console.log('hello there');
+
+const logSomething = () => {
+  console.log('something');
+};
+
