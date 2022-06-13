55dfd47312337764dec21458c8c9023c716daf14
Rick Ansay <rick.ansay@gmail.com>
2022-06-13 09:45:40 -0600

    Complete character sorter

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
-const numbersCollection = new NumbersCollection([1000, 35645, -5, 0]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([1000, 35645, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('XaabYc');
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
+
