11df7fe350ef7d3c2a487b7b9072d23f4a34ebc1
Rick Ansay <rick.ansay@gmail.com>
2022-06-15 22:19:59 -0600

    Refactor sort method for NumbersCollection and LinkedList

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,18 @@ import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
 // const numbersCollection = new NumbersCollection([1000, 35645, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
+// numbersCollection.sort();
 // console.log(numbersCollection.data);
 
 // const charactersCollection = new CharactersCollection('XaabYc');
-// const sorter = new Sorter(charactersCollection);
-// sorter.sort();
+// charactersCollection.sort();
 // console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
-linkedList.add(50);
+linkedList.add(500);
 linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
